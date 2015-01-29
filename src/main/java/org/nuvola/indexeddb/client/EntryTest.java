37260789046db3bccc6f69ec1fdf719f89674bda
imrabti <imrabti@gmail.com>
2015-01-29 15:04:23 +0000

    Added Empty working GWT project.

---
+++ src/main/java/org/nuvola/indexeddb/client/EntryTest.java
@@ -0,0 +1,11 @@
+package org.nuvola.indexeddb.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+public class EntryTest implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+        Window.alert("Hey There...");
+    }
+}
